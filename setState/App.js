60c962a5953ff82431cd5bde8640f1b43c37443a
cicas.Nicola <nardellanicola10@gmail.com>
2016-08-06 12:58:36 +0200

    widget component

--- setState/App.js
+++ setState/App.js
@@ -4,8 +4,9 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      txt: 'this is the state txt'
-     }
+      txt: ''
+    }
+    this.update = this.update.bind(this)
   }
 
   update(e){
@@ -15,10 +16,18 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-        <h1>{this.state.txt}</h1>
+        <widget txt={this.state.txt} update={this.update} />
       </div>
-    )
+    );
+  }
+
+  const widget = (props) => {
+    return (
+      <div>
+        <input type="text" onChange={props.update} />
+        <h1>{props.txt}</h1>
+      </div>
+    );
   }
 }
 
